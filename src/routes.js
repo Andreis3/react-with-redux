5730094b6148f860f77fa266b6948f3cbdaed6e6
Andreis3 <andrei.as@hotmail.com>
2019-09-01 19:06:39 -0300

    add item in cart

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ import Cart from './pages/Cart';
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" exact componet={Home} />
+      <Route path="/" exact component={Home} />
       <Route path="/cart" component={Cart} />
     </Switch>
   );
