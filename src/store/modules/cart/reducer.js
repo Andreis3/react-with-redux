82696aeb7c1f8a167b19597be6487220c4a54b18
Andreis3 <andrei.as@hotmail.com>
2019-09-10 00:34:06 -0300

    configure redux-saga

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -2,7 +2,7 @@ import produce from 'immer';
 
 export default function cart(state = [], action) {
   switch (action.type) {
-    case '@cart/ADD':
+    case '@cart/ADD_SUCESS':
       // refeito para usar o metodo do inner
       // return [
       //   ...state,
